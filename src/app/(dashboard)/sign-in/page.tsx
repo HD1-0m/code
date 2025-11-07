7b0c0900c0fd8210dbf66c1c1b829cadd9fd9d4e
HD1-0m <hariom118narayangupta@gmail.com>
2025-11-07 22:20:59 +0530

    Delete src directory

--- src/app/(dashboard)/sign-in/page.tsx
+++
@@ -1,6 +0,0 @@
-const LoginPage = () => {
-    return(
-        <div className=''>AdminPage</div>
-    )
-}
-export default LoginPage
--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://etiqnycmxgvdhnydusjs.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV0aXFueWNteGd2ZGhueWR1c2pzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIxNzg5ODcsImV4cCI6MjA3Nzc1NDk4N30.IvwK1buoNMvr-FTLEoP1YHpFTarPVOqiyRPZfTDrynI";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
